2d7da3170d6f018a5d3e4090d242fc9555b678ef
Daniel Gilbert <ich@danielgilbert.de>
2023-01-02 23:39:11 +0100

    Adds chrome extension

---
+++ Chrome/settings/options.js
@@ -0,0 +1,50 @@
+// Saves options to chrome.storage
+async function save_options() {
+    var entropy = document.getElementById('entropy').value;
+    var schedule = JSON.parse(document.getElementById('schedule').value);
+    var allowPrefill = document.getElementById('allowPrefill').checked;
+
+    await saveObjectInLocalStorage('ENTROPY_MINUTES', entropy);
+    await saveObjectInLocalStorage('SCHEDULE', schedule);
+    await saveObjectInLocalStorage('ALLOW_PREFILL', allowPrefill);
+
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+        status.textContent = '';
+    }, 750);
+}
+
+async function reset_to_defaults() {
+    await saveObjectInLocalStorage('ENTROPY_MINUTES', DEFAULT_ENTROPY_MINUTES);
+    await saveObjectInLocalStorage('SCHEDULE', DEFAULT_SCHEDULE);
+    await saveObjectInLocalStorage('ALLOW_PREFILL', DEFAULT_ALLOW_PREFILL);
+
+    await restore_options();
+
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options restored.';
+    setTimeout(function() {
+        status.textContent = '';
+    }, 750);
+}
+  
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+async function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    let entropy_minutes = await getObjectFromLocalStorage('ENTROPY_MINUTES');
+    let schedule = JSON.stringify(await getObjectFromLocalStorage('SCHEDULE'), null, 2);
+    let allowPrefill = await getObjectFromLocalStorage('ALLOW_PREFILL');
+
+    document.getElementById('entropy').value = entropy_minutes;
+    document.getElementById('schedule').value = schedule;
+    document.getElementById('allowPrefill').checked = allowPrefill;
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('reset').addEventListener('click', reset_to_defaults);
