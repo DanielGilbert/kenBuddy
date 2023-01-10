79885b914fe38a83366b20b74c17ce08899df49a
Daniel Gilbert <ich@danielgilbert.de>
2023-01-10 18:53:28 +0100

    Finalizes design for settings page

--- extension/settings/options.js
+++ extension/settings/options.js
@@ -1,11 +1,15 @@
+function timeToDate(timeString){
+    return new Date(Date.parse(timeString))
+}
+
 // Saves options to chrome.storage
 async function save_options() {
     var entropy = document.getElementById('entropy').value;
-    var schedule = JSON.parse(document.getElementById('schedule').value);
+    //var schedule = JSON.parse(document.getElementById('schedule').value);
     var allowPrefill = document.getElementById('allowPrefill').checked;
 
     await saveObjectInLocalStorage('ENTROPY_MINUTES', entropy);
-    await saveObjectInLocalStorage('SCHEDULE', schedule);
+    //await saveObjectInLocalStorage('SCHEDULE', schedule);
     await saveObjectInLocalStorage('ALLOW_PREFILL', allowPrefill);
 
     // Update status to let user know options were saved.
@@ -35,26 +39,174 @@ async function set_defaults(){
     await saveObjectInLocalStorage('ALLOW_PREFILL', DEFAULT_ALLOW_PREFILL);
 }
 
+function changeBorderOnClick(element){
+    if (element.checked != null){
+
+    } 
+}
+
+async function createFormCheckRow(weekdayItem){
+    let rowDiv = document.createElement('div');
+    rowDiv.className = "form-group row";
+    
+    let rowMarginDiv = document.createElement('div');
+    rowMarginDiv.className = 'ml-3';
+
+    let rowFormCheckDiv = document.createElement('div');
+    rowFormCheckDiv.className = 'col-sm form-check form-switch';
+
+    let rowFormInputElement = document.createElement('input');
+    rowFormInputElement.className = 'form-check-input';
+    rowFormInputElement.type = 'checkbox';
+    rowFormInputElement.id = weekdayItem + '-isActive';
+    rowFormInputElement.setAttribute('aria-describedby', weekdayItem + '-isActiveHelp');
+    rowFormInputElement.addEventListener('click', changeBorderOnClick);
+
+    let rowFormLabelElement = document.createElement('label');
+    rowFormLabelElement.className = 'form-check-label';
+    rowFormLabelElement.id = weekdayItem + '-isActiveHelp';
+    rowFormLabelElement.setAttribute('for', weekdayItem + '-isActive');
+    rowFormLabelElement.innerText = 'Active';
+
+    rowFormCheckDiv.append(rowFormLabelElement);
+    rowFormCheckDiv.append(rowFormInputElement);
+
+    rowMarginDiv.append(rowFormCheckDiv);
+
+    rowDiv.append(rowMarginDiv);
+
+    return rowDiv;
+}
+
+async function createFormInputRow(weekdayItem, label, type){
+    let rowDiv = document.createElement('div');
+    rowDiv.className = "form-group row";
+    
+    let rowColDiv = document.createElement('div');
+    rowColDiv.className = 'col-sm';
+
+    let rowFormInputElement = document.createElement('input');
+    rowFormInputElement.className = 'form-control';
+    rowFormInputElement.type = 'time';
+    rowFormInputElement.id = weekdayItem + '-' + type + '-timeInput';
+    rowFormInputElement.setAttribute('aria-describedby', weekdayItem + '-' + type + '-timeInputLabel');
+
+    let rowFormLabelElement = document.createElement('label');
+    rowFormLabelElement.className = 'col-4 col-form-label';
+    rowFormLabelElement.id = weekdayItem + '-' + type + '-timeInputLabel';
+    rowFormLabelElement.setAttribute('for', weekdayItem + '-' + type + '-timeInput');
+    rowFormLabelElement.innerText = label;
+
+
+    rowColDiv.append(rowFormInputElement);
+
+    rowDiv.append(rowFormLabelElement);
+    rowDiv.append(rowColDiv);
+
+    return rowDiv;
+}
+
+async function createScheduleLayout(){
+    const weekdayCards = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    const timeEdits = ["start", "pause", "hours"];
+
+    for (let weekdayItem of weekdayCards){
+        let item = document.getElementById(weekdayItem + '-card');
+
+        let cardDiv = document.createElement('div');
+        cardDiv.className = 'card border-primary mb-3';
+        
+        let cardDivHeader = document.createElement('div');
+        cardDivHeader.className = 'card-header';
+        cardDivHeader.innerText = weekdayItem;
+
+        var cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        
+        var formCheckRow = await createFormCheckRow(weekdayItem);
+        cardBody.append(formCheckRow);
+
+        for (let timeItem of timeEdits){
+            var inputRow = await createFormInputRow(weekdayItem, timeItem, timeItem);
+            cardBody.append(inputRow)
+        }
+
+        cardDiv.append(cardDivHeader);
+        cardDiv.append(cardBody);
+
+        item.append(cardDiv);
+    }
+}
+
+function padTimeIfNeeded(time){
+    let values = time.split(':');
+    
+    let hours = parseInt(values[0]);
+    let minutes = parseInt(values[1]);
+    
+    let hoursResult = hours < 10 ? '0' + hours : hours;
+    let minutesResult = minutes < 10 ? '0' + minutes : minutes;
+    
+    return hoursResult + ':' + minutesResult;
+}
+
+async function setScheduleForCard(scheduleItem, weekday){
+    var activeSwitch = document.getElementById(weekday + '-isActive');
+    activeSwitch.checked = 'checked';
+
+    var startInput =  document.getElementById(weekday + '-start-timeInput');
+    startInput.value = padTimeIfNeeded(scheduleItem[0].start);
+
+    var pauseInput =  document.getElementById(weekday + '-pause-timeInput');
+    pauseInput.value = padTimeIfNeeded(scheduleItem[0].pause);
+
+    var hoursInput =  document.getElementById(weekday + '-hours-timeInput');
+    hoursInput.value = padTimeIfNeeded(scheduleItem[0].hours);
+}
+
+async function setSchedule(schedule){
+    let weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+    var i = 0;
+
+    for(let singleWeekday of weekdays){
+        if (schedule[i] == null){
+            i++;
+            continue;
+        }
+       
+        await setScheduleForCard(schedule[i], singleWeekday);
+        i++;
+    }
+}
+
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 async function restore_options() {
     let entropy_minutes = await getObjectFromLocalStorage('ENTROPY_MINUTES');
-    let schedule = JSON.stringify(await getObjectFromLocalStorage('SCHEDULE'), null, 2);
+    let schedule = await getObjectFromLocalStorage('SCHEDULE');
     let allowPrefill = await getObjectFromLocalStorage('ALLOW_PREFILL');
 
     if (entropy_minutes == null || schedule == null || allowPrefill == null){
         await set_defaults();
 
         entropy_minutes = await getObjectFromLocalStorage('ENTROPY_MINUTES');
-        schedule = JSON.stringify(await getObjectFromLocalStorage('SCHEDULE'), null, 2);
+        schedule = await getObjectFromLocalStorage('SCHEDULE');
         allowPrefill = await getObjectFromLocalStorage('ALLOW_PREFILL');
     }
 
     document.getElementById('entropy').value = entropy_minutes;
-    document.getElementById('schedule').value = schedule;
+    await setSchedule(schedule);
+    //document.getElementById('schedule').value = schedule;
     document.getElementById('allowPrefill').checked = allowPrefill;
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
+async function domLoaded(){
+    await createScheduleLayout();
+    await restore_options();
+}
+
+document.addEventListener('DOMContentLoaded', domLoaded);
+
 document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('reset').addEventListener('click', reset_to_defaults);
